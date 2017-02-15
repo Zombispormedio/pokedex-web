faf1a697854a9eada72799d9e70cb4df28621d18
Xavier Serrano <Xavier Serrano>
2017-02-15 22:36:42 +0100

    postinstall

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.listen(port, () => {
     console.log(`Server running on ${port}`);
 });
 
-// "postinstall": "webpack --config webpack.config.prod.js",
+// "postinstall": "webpack --config webpack.config.js",
