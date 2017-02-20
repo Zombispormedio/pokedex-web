3463151b42f24bacf1f76c746d9f3cd6fe496bf7
Xavier Serrano <Xavier Serrano>
2017-02-20 09:22:04 +0100

    divider

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,13 +6,12 @@ import styles from '../theme/styles.scss';
 
 
 const ItemList = ({items, onItemClick})=>{
-    const list = items.map((p)=>
-        <PokeItem 
-            key={p.id.toString()} 
-            pokemon={p} 
-            onClick={() => onItemClick(p.id)}
-        />
-    )
+    const list = items.map( p => (
+        <div key={p.id.toString()}>
+            <PokeItem pokemon={p} onClick={() => onItemClick(p.id)}/>
+            <div className={styles.divider}></div>
+        </div>
+    ))
     return (
         <div className={styles.itemList}>
            {list}
